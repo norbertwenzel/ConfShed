1002aac2d12e35a357fb4f0f9b3942a47930a13c
Norbert Wenzel <norbert.wenzel@gmail.com>
2014-01-18 09:58:45 +0100

    changed main file to allow to register own C++ classes into qml

--- src/ConfSched.cpp
+++ src/ConfSched.cpp
@@ -28,6 +28,10 @@
   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
+#include <QObject>
+#include <QGuiApplication>
+#include <QQmlEngine>
+#include <QScopedPointer>
 #ifdef QT_QML_DEBUG
 #include <QtQuick>
 #endif
@@ -37,15 +41,18 @@
 
 int main(int argc, char *argv[])
 {
-    // SailfishApp::main() will display "qml/template.qml", if you need more
-    // control over initialization, you can use:
-    //
-    //   - SailfishApp::application(int, char *[]) to get the QGuiApplication *
-    //   - SailfishApp::createView() to get a new QQuickView * instance
-    //   - SailfishApp::pathTo(QString) to get a QUrl to a resource file
-    //
-    // To display the view, call "show()" (will show fullscreen on device).
+    QScopedPointer<QGuiApplication> app(SailfishApp::application(argc, argv));
+    //qmlRegisterType<sft::event>("org.fosdem.events", 0, 1, "ConfEvent");
 
-    return SailfishApp::main(argc, argv);
+    QScopedPointer<QQuickView> view(SailfishApp::createView());
+
+    //set the c++ data as a model
+    //sft::event_list_model event_list(get_data());
+    //view->rootContext()->setContextProperty("eventModel", &event_list);
+
+    view->setSource(SailfishApp::pathTo("qml/ConfSched.qml"));
+    view->show();
+
+    return app->exec();
 }
 
